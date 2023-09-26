29c8dcb31512c394c7adc6e54997724401741d5e
Dani <daniel.rebollar.medina@alumnos.upm.es>
2023-09-26 20:17:04 +0200

    Create test and functional command

--- extensions/commands/compliance/cmd_collect_licenses.py
+++ extensions/commands/compliance/cmd_collect_licenses.py
@@ -7,6 +7,7 @@ from conan.cli.command import conan_command
 from conan.cli.formatters.graph import format_graph_json
 from conan.cli.printers import print_profiles
 from conan.cli.printers.graph import print_graph_packages, print_graph_basic
+from conan.tools.files import copy, rmdir
 
 
 @conan_command(group="Compliance", formatters={"json": format_graph_json})
@@ -17,6 +18,8 @@ def collect_licenses(conan_api, parser, *args):
     on a licenses.txt file in the same folder.
     """
 
+    out = ConanOutput("deployer(licenses)")
+    # Everything based in install command and in conan-extensions\extensions\deployers\licenses.py
     common_graph_args(parser)
     parser.add_argument("-g", "--generator", action="append",
                         help='Generators to use')
@@ -47,9 +50,11 @@ def collect_licenses(conan_api, parser, *args):
                                                partial=args.lockfile_partial, overrides=overrides)
     profile_host, profile_build = conan_api.profiles.get_profiles_from_args(
         args)
+    # I keep this because I asume the information could be usefull, as it is in the install command
     print_profiles(profile_host, profile_build)
 
     # Graph computation (without installation of binaries)
+    # I keep this full part, because is requested in the assignment
     gapi = conan_api.graph
     if path:
         deps_graph = gapi.load_graph_consumer(path, args.name, args.version, args.user, args.channel,
@@ -63,4 +68,34 @@ def collect_licenses(conan_api, parser, *args):
     gapi.analyze_binaries(deps_graph, args.build, remotes,
                           update=args.update, lockfile=lockfile)
     print_graph_packages(deps_graph)
-    return 0
+
+    # Installation of binaries and consumer generators
+    # I keep this for convenience, this way I dont have to install and then run the command
+    # Also needed for copy to work
+    conan_api.install.install_binaries(deps_graph=deps_graph, remotes=remotes)
+
+    conanfile = deps_graph.root.conanfile
+    files = []
+
+    # I will create the folder with base in the current directory
+    tmp_dir = os.path.join(cwd, "licenses")
+    if os.path.exists(tmp_dir):
+        rmdir(conanfile, tmp_dir)
+
+    for r, d in conanfile.dependencies.items():
+        if d.package_folder is None:
+            continue
+        search_dir = os.path.join(d.package_folder, "licenses")
+        if not os.path.isdir(search_dir):
+            continue
+        for f in os.listdir(search_dir):
+            src = os.path.join(search_dir)
+            dst = os.path.join(tmp_dir, str(d.ref))
+            out.debug(src)
+            out.debug(dst)
+            copy(conanfile, f, src, dst)
+            files.append(os.path.join(str(d.ref), f))
+    if os.path.exists(tmp_dir):
+        with open(cwd + '\\licenses\\licenses.txt', 'w') as f:
+            for license_file in files:
+                f.write(license_file + '\n')
---
+++ tests/test_collect_licenses.py
@@ -0,0 +1,60 @@
+import tempfile
+import textwrap
+import os
+
+import pytest
+
+from tools import save, run
+
+
+@pytest.fixture(autouse=True)
+def conan_test():
+    old_env = dict(os.environ)
+    env_vars = {"CONAN_HOME": tempfile.mkdtemp(suffix='conans')}
+    os.environ.update(env_vars)
+    current = tempfile.mkdtemp(suffix="conans")
+    cwd = os.getcwd()
+    os.chdir(current)
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+        os.environ.clear()
+        os.environ.update(old_env)
+
+
+def test_collect_licenses():
+    run("conan profile detect")
+    repo = os.path.join(os.path.dirname(__file__), "..")
+    run(f"conan config install {repo}")
+    conanfile = textwrap.dedent("""
+        import os
+
+        from conan import ConanFile
+        from conan.tools.cmake import cmake_layout
+        from conan.tools.files import copy
+
+
+        class ImGuiExample(ConanFile):
+            settings = "os", "compiler", "build_type", "arch"
+            generators = "CMakeDeps", "CMakeToolchain"
+
+            def requirements(self):
+                self.requires("imgui/1.89.4")
+                self.requires("glfw/3.3.8")
+                self.requires("glew/2.2.0")
+
+            def generate(self):
+                copy(self, "*glfw*", os.path.join(self.dependencies["imgui"].package_folder,
+                    "res", "bindings"), os.path.join(self.source_folder, "bindings"))
+                copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder,
+                    "res", "bindings"), os.path.join(self.source_folder, "bindings"))
+
+            def layout(self):
+                cmake_layout(self)
+        """)
+
+    save("conanfile.py", conanfile)
+    run("conan compliance:collect-licenses conanfile.py")
+
+    assert 0 == 0
